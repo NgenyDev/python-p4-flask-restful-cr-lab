cea4339f337c9fc0a884c85a42aa3ee6844444b3
Ngeny Gilbert <ngenygkip@gmail.com>
2024-10-11 09:14:07 +0300

    cr-lab

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -7,3 +8,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)  # Fixed db.string to db.String
+    image = db.Column(db.String, nullable=False, default='default_image.png')
+    price = db.Column(db.Numeric(10, 2), nullable=False, default=0.0)  # Fixed db.numeric to db.Numeric
